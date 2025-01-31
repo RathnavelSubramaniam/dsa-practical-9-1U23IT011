008fc2926141fe6f53aafeb628c8c3d6ad5315e4
1U23IT011 <141386217+1U23IT011@users.noreply.github.com>
2025-01-31 05:10:55 +0000

    Done9

--- LinkedList.java
+++ LinkedList.java
@@ -1,19 +1,20 @@
 import java.util.Scanner;
+
 public class LinkedList {
     class Node {
-    int data;
-    Node next;
- 
-    public Node(int data) {
-        this.data = data;
-        this.next = null;
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
         }
     }
- 
+
     Node head = null;
     Node tail = null;
- 
-public void addNode(int data) {
+
+    public void addNode(int data) {
         Node newNode = new Node(data);
         if (head == null) {
             head = newNode;
@@ -23,37 +24,46 @@ public void addNode(int data) {
             tail = newNode;
         }
     }
- 
-public static void main(String[] args) {
-LinkedList list = new LinkedList();
-Scanner sc = new Scanner(System.in);
- 
-System.out.println("Enter integers to create nodes (enter a non-integer to exit):");
- 
-while (sc.hasNextInt()) {
-int data = sc.nextInt();
-list.addNode(data);
+
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter integers to create nodes (enter a non-integer to exit):");
+
+        while (sc.hasNextInt()) {
+            int data = sc.nextInt();
+            list.addNode(data);
         }
-System.out.println("Linked List created");
- 
-System.out.println("Linked List Before Deleting");
-list.displayList();
-System.out.println("Deletion at Beginning");
-list.deleteBegin();
-System.out.println("Linked List After Deleting at Beginning");
-list.displayList();
-sc.close();
+        System.out.println("Linked List created");
+
+        System.out.println("Linked List Before Deleting");
+        list.displayList();
+        System.out.println("Deletion at Beginning");
+        list.deleteBegin();
+        System.out.println("Linked List After Deleting at Beginning");
+        list.displayList();
+        sc.close();
     }
-public void deleteBegin()
-{
-// TYPE YOUR CODE HERE
-}
-public void displayList() {
+
+    public void deleteBegin() {
+        if (head == null) {
+            return; // Nothing to delete if the list is empty
+        }
+
+        head = head.next; // Move head to the next node
+
+        if (head == null) {  //Check if the list became empty
+            tail = null; // If the list becomes empty, update tail as well
+        }
+    }
+
+    public void displayList() {
         Node current = head;
         while (current != null) {
-        System.out.print(current.data + " -> ");
-        current = current.next;
+            System.out.print(current.data + " -> ");
+            current = current.next;
         }
-System.out.println("null");
+        System.out.println("null");
     }
- }
+}
